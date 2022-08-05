782bb218ec80736e705589152cddf12fa035e345
Chenying Zhao <chenyzh@seas.upenn.edu>
2022-08-05 18:19:12 -0400

    add how to install datalad on Mac M1

--- install_datalad.sh
+++ install_datalad.sh
@@ -14,11 +14,21 @@ conda activate ${conda_env}
 
 # Step 2. Install datalad:
 # in this new env, install datalad, git, and git-annex:
+# [ON LINUX]: install in one command as below:
 conda install -c conda-forge datalad git git-annex
+# [ON MAC with M1 chip]: 
+# 1. install `git-annex` from source using `brew`:
+brew install --build-from-source git-annex 
+# 2. install datalad:
+pip install datalad   # M1 Mac; see here for non-M1 Mac: https://handbook.datalad.org/en/latest/intro/installation.html#mac-osx-non-m1
+# it seems installing git again is not necessary as long as it is on the system.
 
 # check the versions:
 datalad --version
 git --version
+
+# check git-annex is well installed:
+git-annex
 
 # try out datalad to confirm it's error free:
 datalad create -c text2git my-dataset
